32ea1c8f6e70581c490e485b945e84d7948ce9f0
priyavarshini-mediwave <priyavarshini@mindwaveventures.com>
2023-09-25 19:00:38 +0530

    Quiz App -full completed.

--- README.md
+++ README.md
@@ -5,3 +5,6 @@
 #### Display question according to category.
 
 ### On submit show crt ans, change colour according to correctness
+### will stand on refresh
+Local storage function
+#### Using only one JSON
--- app/compscript.js
+++ app/compscript.js
@@ -1,77 +1,173 @@
-Computer = [
-  {
-    id: 1,
-    question: "Who is the father of the Computer?",
-    options: [
-      "Bjarne Stroustrup",
-      "Charles Babbage",
-      "Guido Van Rossum",
-      "Vasco Da Gama",
-    ],
-    answer: "Charles Babbage",
-  },
-  {
-    id: 2,
-    question: "Who invented C++?",
-    options: [
-      "Bjarne Stroustrup",
-      "Charles Babbage",
-      "Guido Van Rossum",
-      "Vasco Da Gama",
-    ],
-    answer: "Bjarne Stroustrup",
-  },
-  {
-    id: 3,
-    question: "Choose the OOPs language.",
-    options: ["HTML", "C", "Basic", "JS"],
-    answer: "JS",
-  },
-];
+const quiz = {
+  //categories
+  categories: [
+    {
+      id: 1,
+      name: "ComputerScience",
+      value: "computer",
+    },
 
-const Geology = [
-  {
-    id: "104",
-    question: "Which is the highest peak? ",
-    options: ["Mt.Everest", "Mt.Sanas", "Mt.Alps", "Mt.Kangchenjunga"],
-    Crtanswer: "Mt.Everest",
-    category: 2,
-  },
-  {
-    id: "105",
-    question: "Which is the outer most layer of the earth?",
-    options: ["Mantle", "Crust", "Core", "InnerCore"],
-    Crtanswer: "Crust",
-    category: 2,
-  },
-  {
-    id: "106",
-    question: "What is the thin layer of gases that surrounds the Earth?",
-    options: ["Lithosphere", "Hydrosphere", "Atmosphere", "Biosphere"],
-    Crtanswer: "Atmosphere",
-    category: 2,
-  },
-];
-const questionCollection = {
-  Computer,
-  Geology,
+    {
+      id: 2,
+      name: "Geology",
+      value: "geology",
+    },
+  ],
+
+  questions: [
+    {
+      id: "101",
+      question: "Who is the father of Computer?",
+      options: [
+        { id: "111", answer: "Charles Babbage", isCorrect: true },
+        { id: "112", answer: "Dennis Ritchie", isCorrect: false },
+        { id: "113", answer: "Guido Van Russom", isCorrect: false },
+        { id: "114", answer: "Bjane Stroustrup", isCorrect: false },
+      ],
+
+      category: 1,
+    },
+    {
+      id: "102",
+      question: "Who founded C++?",
+      options: [
+        { id: "121", answer: "Charles Babbage", isCorrect: false },
+        { id: "122", answer: "Dennis Ritchie", isCorrect: false },
+        { id: "123", answer: "Guido Van Russom", isCorrect: false },
+        { id: "124", answer: "Bjarne Stroustrup", isCorrect: true },
+      ],
+
+      category: 1,
+    },
+    {
+      id: "103",
+      question: "Which is a OOPs language?",
+      options: [
+        { id: "131", answer: "HTML", isCorrect: false },
+        { id: "132", answer: "C", isCorrect: false },
+        { id: "133", answer: "Basic", isCorrect: false },
+        { id: "134", answer: "Js", isCorrect: true },
+      ],
+      category: 1,
+    },
+    {
+      id: "104",
+      question: "Which is the highest peak? ",
+      options: [
+        { id: "141", answer: "Mt.Everest", isCorrect: true },
+        { id: "142", answer: "Mt.Sanas", isCorrect: false },
+        { id: "143", answer: "Mt.Alps", isCorrect: false },
+        { id: "144", answer: "Mt.Kangchenjunga", isCorrect: false },
+      ],
+
+      category: 2,
+    },
+    {
+      id: "105",
+      question: "Which is the outer most layer of the earth?",
+      options: [
+        { id: "151", answer: "Mantle", isCorrect: false },
+        { id: "152", answer: "Crust", isCorrect: true },
+        { id: "153", answer: "Core", isCorrect: false },
+        { id: "154", answer: "InnerCore", isCorrect: false },
+      ],
+
+      // options: ["Mantle", "Crust", "Core", "InnerCore"],
+      // Crtanswer: "Crust",
+      category: 2,
+    },
+    {
+      id: "106",
+      question: "What is the thin layer of gases that surrounds the Earth?",
+      options: [
+        { id: "161", answer: "Lithosphere", isCorrect: true },
+        { id: "162", answer: "Hydrosphere", isCorrect: false },
+        { id: "163", answer: "Atmosphere", isCorrect: true },
+        { id: "164", answer: "Biosphere", isCorrect: false },
+      ],
+      // options: ["Lithosphere", "Hydrosphere", "Atmosphere", "Biosphere"],
+      // Crtanswer: "Atmosphere",
+      category: 2,
+    },
+  ],
 };
+function appendtoselectInput() {
+  for (i = 0; i < quiz.categories.length; i++) {
+    const option = document.createElement("option");
+    option.setAttribute("value", quiz.categories[i].value);
+    option.innerText = quiz.categories[i].name;
+    const selectInput = document.querySelector("#category-input");
+    selectInput.appendChild(option);
+  }
+}
+appendtoselectInput();
+function loadfromLocal(selectedValue) {
+  setLocalStorageItem("selectedCategory", selectedValue);
+  let selectedValueIndex = quiz.categories.findIndex(
+    (item) => item.value == selectedValue
+  );
+  let selectedId = quiz.categories[selectedValueIndex].id;
 
-const urlParams = new URLSearchParams(window.location.search);
-const myType = urlParams.get("type");
-console.log(questionCollection[myType]);
-const subjectarray = questionCollection[myType];
+  form.style.display = "none";
+  const appWrap = document.querySelector("#app-wrap");
+  appWrap.style.display = "block";
+  const app = document.createElement("div");
+  app.setAttribute("id", "app");
+  const buttonsdiv = document.createElement("div");
+  buttonsdiv.setAttribute("class", "buttonsdiv");
+  buttonsdiv.setAttribute("id", "buttonsdiv");
+  appWrap.appendChild(app);
+  appWrap.appendChild(buttonsdiv);
+  appendtoButtonsdiv();
+  buttonsdiv.style.display = "block";
+  console.log(selectedId);
+  updateUI(selectedId);
+}
+form = document.querySelector("#choose-category-form");
+const btnproceed = document.querySelector("#proceed-btn");
+btnproceed.addEventListener("click", function (e) {
+  e.preventDefault();
+  const selectInput = document.querySelector("#category-input");
+  let selectedValue = selectInput.value;
+  loadfromLocal(selectedValue);
+});
 
-//for (let mcq of questionCollection[myType])
+function appendtoButtonsdiv() {
+  const buttonsdiv = document.querySelector("#buttonsdiv");
 
-function updateUI() {
+  const buttonback = document.createElement("button");
+  buttonback.setAttribute("type", "submit");
+  buttonback.setAttribute("class", "buttonback");
+  buttonback.setAttribute("id", "buttonback");
+  buttonback.innerText = "Back";
+  buttonback.addEventListener("click", function () {
+    Moveback();
+  });
+
+  const buttonsubmit = document.createElement("button");
+  buttonsubmit.setAttribute("type", "submit");
+  buttonsubmit.setAttribute("class", "buttonsubmit");
+  buttonsubmit.setAttribute("id", "buttonSubmit");
+  buttonsubmit.innerText = "Submit";
+
+  buttonsubmit.addEventListener("click", function (e) {
+    alert("Submitting...");
+  });
+
+  buttonsdiv.appendChild(buttonback);
+  buttonsdiv.appendChild(buttonsubmit);
+}
+
+function updateUI(selectedId) {
   clearapp();
-  for (let item of subjectarray) {
-    console.log(item);
-    const choose = makeChoosediv(item);
+  //favMovies.filter((movie) => movie.id != movieId)
+  const questions = quiz.questions.filter(
+    (item) => item.category == selectedId
+  );
+  console.log(questions);
+  for (let i of questions) {
+    const choose = makeChoosediv(i);
     appendtoapp(choose);
-    //appendtoapp(buttonback);
-    appendtoapp(buttonsdiv);
   }
 }
 function clearapp() {
@@ -92,60 +188,76 @@ function makeChoosediv(item) {
     radiobtn.setAttribute("type", "radio");
     radiobtn.setAttribute("name", `ans-${item.id}`);
 
-    radiobtn.value = item.options[i];
+    radiobtn.value = item.options[i].answer;
 
-    const radioname = document.createTextNode(item.options[i]);
+    const radioname = document.createTextNode(item.options[i].answer);
     div.appendChild(radiobtn);
     div.appendChild(radioname);
   }
   const resultdiv = document.createElement("div");
   resultdiv.setAttribute("class", "resultdiv");
-  resultdiv.setAttribute("id", `resultdiv${i}`);
+  resultdiv.setAttribute("id", `resultdiv${item.id}`);
   resultdiv.innerText = "result";
   div.appendChild(resultdiv);
+
+  const submitBtn = document.querySelector("#buttonSubmit");
+
+  submitBtn.addEventListener("click", function () {
+    // let selectedname = `ans-${item.id}`;
+    //console.log(selectedname);
+    const selected = document.querySelector(
+      `input[name="ans-${item.id}"]:checked`
+    );
+
+    console.log(selected);
+    //console.log(selected.value);
+    // const checkAnswer = item.options[i];
+    // console.log(checkAnswer);
+    console.log("hi");
+
+    if (selected) {
+      const choosenAnswer = selected.value;
+      const answerIndex = item["options"].findIndex(
+        (item) => item.answer === choosenAnswer
+      );
+      const crtansId = item.options.findIndex((ans) => ans.isCorrect === true);
+      const crtansname = item.options[crtansId].answer;
+      if (item["options"][answerIndex].isCorrect) {
+        correctAnsShow(crtansname, `${item["id"]}`, "green");
+      } else {
+        correctAnsShow(crtansname, `${item["id"]}`, "red");
+      }
+    } else {
+      //const choosenAnswer = selected.value;
+      const answerIndex = item["options"].findIndex(
+        (item) => item.isCorrect === true
+      );
+      const crtansId = item.options.findIndex((ans) => ans.isCorrect === true);
+      const crtansname = item.options[crtansId].answer;
+      correctAnsShow(crtansname, `${item["id"]}`, "orange");
+    }
+  });
   return div;
 }
-const buttonback = document.createElement("button");
-buttonback.setAttribute("type", "submit");
-buttonback.setAttribute("class", "buttonback");
-buttonback.setAttribute("id", "buttonback");
-buttonback.innerText = "Back";
-buttonback.addEventListener("click", function () {
-  Moveback();
-});
-
-const buttonsubmit = document.createElement("button");
-buttonsubmit.setAttribute("type", "submit");
-buttonsubmit.setAttribute("class", "buttonsubmit");
-buttonsubmit.setAttribute("id", "buttonsubmit");
-buttonsubmit.innerText = "Submit";
-buttonsubmit.addEventListener("click", function (e) {
-  e.preventDefault();
-  checkAnswer();
-  compareAnswers();
-});
-const buttonsdiv = document.createElement("div");
-buttonsdiv.setAttribute("class", "buttonsdiv");
-buttonsdiv.setAttribute("id", "buttonsdiv");
-buttonsdiv.appendChild(buttonback);
-buttonsdiv.appendChild(buttonsubmit);
-
-function checkAnswer() {
-  //alert("check for answers");
-  for (i = 0; i < subjectarray.length; i++) {
-    let crtans = document.createElement("p");
-    crtans.setAttribute("class", "crtans");
-    crtans.setAttribute("id", `crtans-${i}`);
-    crtans.innerText = subjectarray[i].answer;
-    console.log(crtans);
-    appendtoapp(crtans);
-  }
+function correctAnsShow(ans, resultId, add) {
+  const divId = `#div-${resultId}`;
+  const divClass = "border-" + add;
+  const div = document.querySelector(divId);
+  div.classList.add(divClass);
+  const selector = `#resultdiv${resultId}`;
+  console.log(selector);
+  const result = document.querySelector(selector);
+  result.innerHTML = "Ans: " + ans;
+  result.classList.add(add);
 }
-
 function Moveback() {
-  console.log("back called");
-  //   history.back();
-  //location.replace("/index.html");
+  const appWrap = document.querySelector("#app-wrap");
+  appWrap.style.display = "none";
+  const buttonsdiv = document.querySelector("#buttonsdiv");
+  buttonsdiv.style.display = "none";
+  setLocalStorageItem("selectedCategory", "");
+  form = document.querySelector("#choose-category-form");
+  form.style.display = "block";
 }
 
 function appendtoapp(choose) {
@@ -153,15 +265,33 @@ function appendtoapp(choose) {
   app.appendChild(choose);
 }
 
-// function compareAnswers() {
-//   //alert("compareAnswers");
-//   const selectedanswers = [];
-//   for (i = 0; i < subjectarray.length; i++) {
-//     let selctedid = `ans-${i}`;
-//     let selected = document.querySelector("#selctedid").value;
-//     selectedanswers.push(selected);
-//   }
-//   console.log(selectedanswers);
-// }
-
-updateUI();
+// Function to set data in localStorage
+function setLocalStorageItem(key, value) {
+  try {
+    localStorage.setItem(key, JSON.stringify(value));
+    return true; // Data successfully stored
+  } catch (error) {
+    console.error("Error storing data in localStorage:", error);
+    return false; // Failed to store data
+  }
+}
+// Function to get data from localStorage
+function getLocalStorageItem(key) {
+  try {
+    const storedValue = localStorage.getItem(key);
+    return storedValue ? JSON.parse(storedValue) : null;
+  } catch (error) {
+    console.error("Error retrieving data from localStorage:", error);
+    return null;
+  }
+}
+window.addEventListener("load", function () {
+  const storedCategory = getLocalStorageItem("selectedCategory");
+  if (storedCategory) {
+    // const selectInput = document.querySelector("#category-input");
+    // selectInput.value = storedCategory;
+    // // Also, you might want to trigger the event as if the user had clicked "proceed" again
+    // document.getElementById("proceed-btn").click();
+    loadfromLocal(storedCategory);
+  }
+});
